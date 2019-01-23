55fa0431bee2afc2eb55382f277fc4d4648941aa
rtuna <38296319+rtunaboss@users.noreply.github.com>
2019-01-23 11:24:51 +0100

    Add files via upload

---
+++ ShopifydelayCalc.py
@@ -0,0 +1,28 @@
+# id 537332882541379596
+# permission 67584
+# https://discordapp.com/oauth2/authorize?client_id=XXXXXXXXXXXX&scope=bot&permissions=XXXXX
+
+# for example: !shopifydelay 50 10 will return 660ms.
+
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '!')
+
+token = open(token.txt,r)
+
+@client.event
+async def on_ready():
+    print('Started the bot.')
+    await client.change_presence(game=discord.Game(name="Calculating..."))
+
+@client.command()
+async def shopifydelay(*args):
+    lst = list(args)
+    proxynum = int(lst[0])
+    tasknum = int(lst[1])
+    # shopify will ban around 3200ms, so I am using 3300ms just to be safe.
+    delay = 3300/proxynum/tasknum *100
+    await client.say(f'Use at a delay around {delay} ms.')
+
+client.run(token)
